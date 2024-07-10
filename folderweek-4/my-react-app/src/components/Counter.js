dd9dd5520cbebea0ce33ead8e28e1a838aa5775e
GwenZi0u0 <a0918566336@gmail.com>
2024-07-11 03:39:25 +0800

    delete

--- folderweek-4/my-react-app/src/components/Counter.js
+++ folderweek-4/my-react-app/src/components/Counter.js
@@ -75,62 +75,6 @@ export default function Counter (){
       <Total total={total} counterTotal={handleTotal} />
     </div> 
   )
-
 }
-
-
-
-//   
-//   const handleInOneCount = (i) => {
-//     let newCount = count.slice()
-//     newCount[i] += 1
-//     setCount(newCount)
-//   }
-
-//   // const [ onCounts, setOnCounts ] = useState(0)
-//   
-//   const handleInCounts = (inCounter) => {
-//     inCounter = count.map(c => c + 1)
-//     setCount(inCounter)
-//     setOneCount(oneCount + 1)
-//   }
-
-//   // 按下後三個按鈕旁的數值加總
-//  const counterTotal = () =>{
-//    setCount([...count, 0])
-//  }
-
-// function Increment({ i, oneCount, inOneCount}){
-
-//   const [count, setCount] = useState([0, 0, 0])
-
-//   const handleInOneCount = () => {
-//     i = count + 1 
-//     setCount(i)
-//     inOneCount(i)
-//   }
-
-//   // const handleInCounts = () =>{
-//   //   const inCounter = count.slice()
-//   //   inCounter.map(x => x + 1 )
-//   //   setCount(inCounter)
-//   // }
-
-//   return(
-//     <>
-//       {/* <div className="allCountBtn">
-//           <button id="allBtn" type="button" onClick={handleInCounts}>All + 1</button>
-//       </div> */}
-//       <div className="sections">
-//         <div className="btns">
-//         <button className="btn" type="button" onClick={handleInOneCount}><span> + 1</span></button>
-//         <p className="result">{oneCount}</p>
-//         </div>
-//       </div>
-//     </>
-//   )
-// }
-
-
 
 
