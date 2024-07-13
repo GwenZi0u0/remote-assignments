6ccade08bf12b4b291727080393d365d349aad38
GwenZi0u0 <a0918566336@gmail.com>
2024-07-13 12:49:20 +0800

    fix Total => AddCounter 將功能改成按下按鈕新增+1按鈕

--- folderweek-4/my-react-app/src/components/Counter.js
+++ folderweek-4/my-react-app/src/components/Counter.js
@@ -1,80 +1,76 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function AddAll({onAllClick}){
-  return(
+function AddAll({ onAllClick }) {
+  return (
     <div className="allCountBtn">
-      <button id="allBtn" type="button" onClick={onAllClick}>All + 1</button>
+      <button id="allBtn" type="button" onClick={onAllClick}>
+        All + 1
+      </button>
     </div>
-  )
+  );
 }
 
-function Add({ onAddClick}){
-  return(
+function Add({ onAddClick }) {
+  return (
     <>
-      <button className="btn" type="button" onClick={onAddClick}><span> + 1</span></button>
+      <button className="btn" type="button" onClick={onAddClick}>
+        <span> + 1</span>
+      </button>
     </>
-  )
+  );
 }
 
-function MyValue({ value }){
-  return(
-    <p className="result">{value}</p>
-  )
+function MyValue({ value }) {
+  return <p className="result">{value}</p>;
 }
 
-function Total({ total, counterTotal }){
-  return(
-     <>
+function AddCounter({ addCounter }) {
+  return (
+    <>
       <div className="btnEnter">
-        <button className="btn addbtn" type="button" onClick={counterTotal}><span>Add a Counter</span></button>
-        <p className="total">{total}</p>
+        <button className="btn addbtn" type="button" onClick={addCounter}>
+          <span>Add a Counter</span>
+        </button>
       </div>
     </>
-  )
+  );
 }
 
-export default function Counter (){
-  const [ values, setValues ] = useState([0, 0, 0])
-
-  const [total,setTotal] =useState(0)
+export default function Counter() {
+  const [values, setValues] = useState([0, 0, 0]);
 
   // 按下後數值加1
-  function handleAddCount(){
-    const addValue = values.map(x => x + 1)
-    setValues(addValue)
+  function handleAddCount() {
+    const addValue = values.map((x) => x + 1);
+    setValues(addValue);
   }
 
   // 按下後三個按鈕旁的數值同時加1
-  function handleOneCount(i){
-    const addOneValues = values.slice()
-    addOneValues[i] += 1
-    setValues(addOneValues)
+  function handleOneCount(i) {
+    const addOneValues = values.slice();
+    addOneValues[i] += 1;
+    setValues(addOneValues);
   }
 
-  function handleTotal(){
-    let count = 0
-    values.forEach( (x) => {
-      count += x
-    } )
-    setTotal(count)
+  // 新增+1按鈕
+  function handleAddCounter() {
+    setValues([...values, 0]);
   }
 
-  return(
+  return (
     <div className="section_all">
       <div className="section_content">
         <AddAll onAllClick={handleAddCount} />
         <div className="sections">
           {values.map((value, index) => (
-            <div className="btns"  key={index}>
+            <div className="btns" key={index}>
               <Add index={index} onAddClick={() => handleOneCount(index)} />
               <MyValue value={value} />
             </div>
           ))}
         </div>
       </div>
-      <Total total={total} counterTotal={handleTotal} />
-    </div> 
-  )
+      <AddCounter addCounter={handleAddCounter} />
+    </div>
+  );
 }
-
-
