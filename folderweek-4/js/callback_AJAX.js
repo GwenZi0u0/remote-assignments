5faa0b15bbc2d5708b128dd82fc07c58ab190b81
GwenZi0u0 <a0918566336@gmail.com>
2024-07-13 12:38:36 +0800

    fix result的結果 => n1+n2 not =>sum * (n1+n2)

--- folderweek-4/js/callback_AJAX.js
+++ folderweek-4/js/callback_AJAX.js
@@ -3,13 +3,10 @@
 // You should use window.setTimeout() for time scheduling.
 
 function delayedResult(n1, n2, delayTime, callback) {
-    setTimeout(function(sum) {
-        sum = ( n1 + n2 )
-        // console.log(sum * ( n1 + n2 ))
-        let result = sum * sum
-        callback(result)
-    },delayTime)  
-           
+  setTimeout(function (sum) {
+    let result = n1 + n2;
+    callback(result);
+  }, delayTime);
 }
 
 // delayedResult(4, 5, 3000, function(result) {
@@ -19,37 +16,39 @@ function delayedResult(n1, n2, delayTime, callback) {
 // window.alert(result);
 // }); // 5 (-5+10) will be shown in an alert dialog after 2 seconds
 
-
 // Assignment 2: HTML DOM and AJAX
 // Complete the functions below to make an AJAX call to a URL by Fetch API, and show the
 // response data in the page. You may render UI with any style
 
 async function ajax(url) {
-    try{
-        const response = await fetch(url)
-        if(!response.ok) {throw new Error(`HTTP: ${response.statusText}`)}
-        
-        const data = await response.json()
-        return data
-    }catch(err){
-        console.log(`ERROR`)
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP: ${response.statusText}`);
     }
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(`ERROR`);
+  }
 }
 
 function render(data) {
-    // console.log(data)
-    const html = document.getElementsByTagName("main")[0]
-    data.forEach(function(products){
-        html.innerHTML += `
+  // console.log(data)
+  const html = document.getElementsByTagName("main")[0];
+  data.forEach(function (products) {
+    html.innerHTML += `
         <div class="card">
             <hander>${products.name}</hander>
             <span>${products.description}</span>
             <footer>價格: ${products.price}</footer>
         </div>
-        `
-    })
+        `;
+  });
 }
-const url = "https://remote-assignment.s3.ap-northeast-1.amazonaws.com/products"
+const url =
+  "https://remote-assignment.s3.ap-northeast-1.amazonaws.com/products";
 ajax(url).then((data) => {
-    render(data);
-})
+  render(data);
+});
