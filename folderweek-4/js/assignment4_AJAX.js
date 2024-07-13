8853fc5ebab6cf780230d36883efa01e23b1ec88
GwenZi0u0 <a0918566336@gmail.com>
2024-07-13 12:35:51 +0800

    fix API 多餘的程式設計 & prettier重整排版

--- folderweek-4/js/assignment4_AJAX.js
+++ folderweek-4/js/assignment4_AJAX.js
@@ -1,61 +1,69 @@
-const card = document.getElementsByTagName("section")[0]
-const moreButton =document.getElementsByTagName("button")[0]
+const card = document.getElementsByTagName("section")[0];
+const moreButton = document.getElementsByTagName("button")[0];
 
 async function ajax(url) {
-    try{
-        const response = await fetch(url)
-        if(!response.ok) {throw new Error(`HTTP: ${response.statusText}`)}
-        
-        const data = await response.json()
-        return data
-    }catch(err){
-        console.log(`ERROR`)
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP: ${response.statusText}`);
     }
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(`ERROR`);
+  }
 }
 
-function lazyload(count){
-    let url = " https://api.github.com/orgs/facebook/repos?per_page=5&page=" + count
-    ajax(url).then((data) => {
-        render(data);
-    })
+function lazyload(count) {
+  let url =
+    " https://api.github.com/orgs/facebook/repos?per_page=5&page=" + count;
+  ajax(url).then((data) => {
+    render(data);
+  });
 }
 
-let count = 1
+let count = 1;
 
-lazyload(count)
-
+lazyload(count);
 
 function render(data) {
-    let currentIndex = 0
-    const pageLength = 5
+  // let currentIndex = 0
+  // const pageLength = 5
+  // const end = currentIndex + pageLength
+  // 原本設計是要進入頁面時就直接渲染前五個資料，但確實上方的過程比較多餘
 
-    const end = currentIndex + pageLength
-    const pageToLength = data.slice(currentIndex, end)    
-       
-    pageToLength.forEach(function(page){
-        let topicList = ""
-        if(page.topics){
-            topicList = page.topics.map(topic => `<li class="topics">${topic}</li>` ).join('')
-        }
+  // 留下slice來渲染
+  const pageToLength = data.slice();
 
-        card.innerHTML += `
+  pageToLength.forEach(function (page) {
+    let topicList = "";
+    if (page.topics) {
+      topicList = page.topics
+        .map((topic) => `<li class="topics">${topic}</li>`)
+        .join("");
+    }
+
+    card.innerHTML += `
         <div class="Card">
             <div class="page">
                 <span class="pageName"><a href="">${page.name}</a></span>
-                <div class="pageVisibility">${page.visibility[0].toUpperCase() + page.visibility.slice(1)}</div>
+                <div class="pageVisibility">${
+                  page.visibility[0].toUpperCase() + page.visibility.slice(1)
+                }</div>
             </div>
             <div class="description">${page.description}</div>
             <ul class="cardTopics">
                 ${topicList}
             </ul>
         </div>
-        `
-    })
+        `;
+  });
 }
 
-function addCount(){
-    count += 1
-    lazyload(count)
+function addCount() {
+  count += 1;
+  lazyload(count);
 }
 
- moreButton.addEventListener('click', addCount)
+moreButton.addEventListener("click", addCount);
